636e76de9ab9669d8de9214ef194bec597bf42d9
kermit <thomas.stocker92@gmail.com>
2016-05-01 13:42:43 +0200

    Ajout des propriétés de l'objet Annonce

--- annonce.cpp
+++ annonce.cpp
@@ -48,6 +48,42 @@ void Annonce::accept() {
 
 }
 
+char* Annonce::readFileBytes(const char *name)
+{
+    std::ifstream fl(name);
+    fl.seekg( 0, std::ios::end );
+    size_t len = fl.tellg();
+    char *ret = new char[len];
+    fl.seekg(0, std::ios::beg);
+    fl.read(ret, len);
+    fl.close();
+    return ret;
+}
+
+//Constructeur
+Annonce::Annonce(TYPE_BIEN peTypeBien,
+                 TYPE_ANNONCE peTypeAnnonce,
+                 double pdblSurfaceHabitable,
+                 double pdblSuperficieTerrain,
+                 int piNombrePiece,
+                 std::string pstrAdresse1,
+                 std::string pstrAdresse2,
+                 std::string pstrDescription,
+                 double pdblPrix,
+                 char* ptbPhotoContractuelle
+                 ){
+    mTypeBien = peTypeBien;
+    mTypeAnnonce = peTypeAnnonce;
+    mSurfaceHabitable = pdblSurfaceHabitable;
+    mSuperficieTerrain = pdblSuperficieTerrain;
+    mNombrePiece = piNombrePiece;
+    mAdresse1 = pstrAdresse1;
+    mAdresse2 = pstrAdresse2;
+    mDescription = pstrDescription;
+    mPrix = pdblPrix;
+    mPhotoContractuelle = ptbPhotoContractuelle;
+}
+
 Annonce::~Annonce()
 {
     delete ui;
--- annonce.h
+++ annonce.h
@@ -5,6 +5,10 @@
 #include <QString>
 #include <QStringList>
 #include <QMessageBox>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
 
 namespace Ui {
 class Annonce;
@@ -15,10 +19,52 @@ class Annonce : public QDialog
     Q_OBJECT
 
 public:
+    enum TYPE_BIEN{
+        MAISON,
+        APPARTEMENT,
+        CHATEAU,
+        FERME,
+        BUREAU,
+        COMMERCE,
+        LOCAL
+    };
+
+
+    enum TYPE_ANNONCE{
+        VENTE,
+        LOCATION
+    };
+    Annonce(TYPE_BIEN peTypeBien,
+             TYPE_ANNONCE peTypeAnnonce,
+             double pdblSurfaceHabitable,
+             double pdblSuperficieTerrain,
+             int piNombrePiece,
+             std::string pstrAdresse1,
+             std::string pstrAdresse2,
+             std::string pstrDescription,
+             double pdblPrix,
+             char* ptbPhotoContractuelle
+             );
+    char* readFileBytes(const char *name);
+
     explicit Annonce(QWidget *parent = 0);
     ~Annonce();
 
 private:
+    TYPE_BIEN mTypeBien;
+    TYPE_ANNONCE mTypeAnnonce;
+    double mSurfaceHabitable;
+    double mSuperficieTerrain;
+    int mNombrePiece;
+    std::string mAdresse1;
+    std::string mAdresse2;
+    std::string mDescription;
+
+    double mPrix;
+
+    char* mPhotoContractuelle;
+    std::vector<char*> mGallerie;
+
     Ui::Annonce *ui;
     void loadTypes();
     void loadNbPieces();
