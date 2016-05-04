d92c4ee614e0063d339d24224ab2cd3f4c0d4b1d
Anthony Den Drijver <anthodd@gmail.com>
2016-05-04 12:23:02 +0200

    Set UTF8

--- main.cpp
+++ main.cpp
@@ -5,6 +5,10 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     IHMo w;
+
+    // -- Text encoding
+    QTextCodec::setCodecForCStrings((QTextCodec::codecForName("UTF-8")));
+
     w.show();
 
     return a.exec();
